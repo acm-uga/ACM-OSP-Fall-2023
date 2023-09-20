e1dc21cf4f9f16ca8545e16c8c92fcc7a4a09ac9
Cason Pittman <xhiquality@gmail.com>
2023-09-20 15:58:49 -0400

    Created Base template for Jetpack Compose
    Blank canvas for project

---
+++ BusApp/app/src/main/java/edu/uga/acm/osp/MainActivity.kt
@@ -0,0 +1,22 @@
+package edu.uga.acm.osp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.uga.acm.osp.ui.theme.BusAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
