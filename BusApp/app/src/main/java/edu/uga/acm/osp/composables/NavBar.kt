c02c40fe939526d34be745313da9ea949b9f6e01
Plant27 <mdb07032@uga.edu>
2023-11-06 11:54:57 -0500

    Changed unselected icon colors
    Also removed outdated and unnecessary import statements

--- BusApp/app/src/main/java/edu/uga/acm/osp/composables/NavBar.kt
+++ BusApp/app/src/main/java/edu/uga/acm/osp/composables/NavBar.kt
@@ -1,33 +1,22 @@
 package edu.uga.acm.osp.composables
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.Badge
 import androidx.compose.material.BadgedBox
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Route
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Route
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.rounded.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,17 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import edu.uga.acm.osp.nav.HomeScreen
-import edu.uga.acm.osp.nav.Navigation
 import edu.uga.acm.osp.ui.theme.BusAppTheme
 
 // The logic behind the navbar
@@ -62,7 +45,7 @@ fun NavBar(items: List<NavBarItem>, navController: NavController, modifier: Modi
             BottomNavigationItem(
                 selected = selected,
                 selectedContentColor = BusAppTheme.colors.positiveStatus,
-                unselectedContentColor = BusAppTheme.colors.negativeStatus,
+                unselectedContentColor = BusAppTheme.colors.onBackgroundSecondary,
                 onClick = { onItemClick(item) },
                 icon = {
                     Column(horizontalAlignment = CenterHorizontally) {
