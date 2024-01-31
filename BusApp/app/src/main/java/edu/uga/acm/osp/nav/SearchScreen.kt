c12ab42ad262d57fd83fc236a078960badde4ccc
Plant27 <mdb07032@uga.edu>
2024-01-31 12:12:28 -0500

    Button updates
    Added a third button,
    gave the buttons the proper naming, coloring, and position

--- BusApp/app/src/main/java/edu/uga/acm/osp/nav/SearchScreen.kt
+++ BusApp/app/src/main/java/edu/uga/acm/osp/nav/SearchScreen.kt
@@ -3,15 +3,23 @@ package edu.uga.acm.osp.nav
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MenuOpen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import edu.uga.acm.osp.components.IconButton
 import edu.uga.acm.osp.components.TextButton
 import edu.uga.acm.osp.composables.DisplayBox
 import edu.uga.acm.osp.composables.Header
@@ -39,9 +47,21 @@ fun SearchScreen(navController: NavController) {
             // NOTE: Composables put here do not display for some reason.
         }
         // Put composables here instead
-        Row(verticalAlignment = Alignment.Bottom) {
-            TextButton(textButton = "test", colorBackground = Color.Black, colorText = Color.Red)
-            TextButton(textButton = "test2", colorBackground = Color.Black, colorText = Color.Red)
+        Row(verticalAlignment = Alignment.Bottom,
+            modifier = Modifier
+                .height(590.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {// I don't know why this isn't changing the position of the row
+            TextButton(textButton = "Bus Routes", 
+                colorBackground = BusAppTheme.colors.container,
+                colorText = BusAppTheme.colors.onBackgroundPrimary)
+            TextButton(textButton = "Bus Stops", 
+                colorBackground = BusAppTheme.colors.container,
+                colorText = BusAppTheme.colors.onBackgroundPrimary)
+            IconButton(iconName = Icons.Outlined.MenuOpen,
+                colorBackground = BusAppTheme.colors.container,
+                description = "test")
         }
     }
 }
