c5d4fcdeab4773d0542c7d2aaa9a275293a3858f
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-03-05 15:14:19 -0500

    Created HomeCards composable and finished basic version of HomeScreen

--- BusApp/app/src/main/java/edu/uga/acm/osp/nav/HomeScreen.kt
+++ BusApp/app/src/main/java/edu/uga/acm/osp/nav/HomeScreen.kt
@@ -1,28 +1,17 @@
 package edu.uga.acm.osp.nav
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import edu.uga.acm.osp.composables.Header
-import edu.uga.acm.osp.composables.NavBar
-import edu.uga.acm.osp.composables.NotificationComposable
-import edu.uga.acm.osp.composables.TestComposable
+import edu.uga.acm.osp.composables.HomeCard
 import edu.uga.acm.osp.composables.displayNavBar
-import edu.uga.acm.osp.composables.myButton
-import edu.uga.acm.osp.ui.theme.BulldogRed
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -35,11 +24,56 @@ fun HomeScreen(navController: NavController) {
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier
-                .padding(innerPadding),
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            // Put composables here!
+
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item {
+                    HomeCard(
+                        cardName = "Recently Viewed",
+                        stop = listOf("Bus Stop 1", "Bus Route 1", "Bus Route 2"),
+                        desc = listOf(
+                            "Serves R1, R2, +2 more",
+                            "M-F 12:00am-12:00pm",
+                            "Weekends 0:00am - 0:00pm"
+                        ),
+                        badge = listOf("Stop", "R1", "R2"),
+                        routes = listOf(false, true, true)
+                    )
+                }
+
+                item {
+                    HomeCard(
+                        cardName = "Favorites",
+                        stop = listOf("Bus Stop 2", "Bus Stop 3", "Bus Stop 4"),
+                        desc = listOf(
+                            "At Building Name",
+                            "Across from Building Name",
+                            "Behind Building Name"
+                        ),
+                        badge = listOf("Stop", "Stop", "Stop"),
+                        routes = listOf(false, false, false)
+                    )
+                }
+
+                item {
+                    HomeCard(
+                        cardName = "Nearby Stops",
+                        stop = listOf("Bus Stop 2", "Bus Stop 3", "Bus Stop 4"),
+                        desc = listOf(
+                            "At Building Name",
+                            "Across from Building Name",
+                            "Behind Building Name"
+                        ),
+                        badge = listOf("Stop", "Stop", "Stop"),
+                        routes = listOf(false, false, false)
+                    )
+                }
+            }
         }
     }
 }
